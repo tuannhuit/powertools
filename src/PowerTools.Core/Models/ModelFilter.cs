eaa82dfbb53fc32add86f3e3e3afcc79ecd5c668
peter.nguyen <peter.nguyen@hansencx.com>
2025-10-26 12:34:56 +0700

    Provide ModelFilter and Update FontWeight

---
+++ src/PowerTools.Core/Models/ModelFilter.cs
@@ -0,0 +1,26 @@
+﻿using Prism.Mvvm;
+using System.Runtime.CompilerServices;
+
+namespace PowerTools.Core.Models
+{
+    public delegate void ModelFilterHandler(string filterName, object valueChanged);
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public class ModelFilter: BindableBase
+    {
+        protected ModelFilterHandler Handler { get; set; }
+        public ModelFilter(ModelFilterHandler handler)
+        {
+            Handler = handler;
+            RaisePropertyChanged();
+        }
+
+        protected void OnRaisePropertyChanged<T>(T valueChanged = default(T), [CallerMemberName] string propertyName = null)
+        {
+            RaisePropertyChanged(propertyName);
+            this.Handler?.Invoke(propertyName, valueChanged);
+        }
+    }
+}
