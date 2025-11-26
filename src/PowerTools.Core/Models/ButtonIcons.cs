9208524af889a9cee454234aed4d471e724eff8f
peter.nguyen <peter.nguyen@hansencx.com>
2025-11-26 20:48:51 +0700

    Update modules information

--- src/PowerTools.Core/Models/ButtonIcons.cs
+++ src/PowerTools.Core/Models/ButtonIcons.cs
@@ -15,6 +15,7 @@
         public static string Check = "\ue081";
         public static string Books = "📚";
         public static string ServerSide = "\ue2E0";
+        public static string PenEdit = "\ue0D8";
         public static string Char_K = "𝞙";
         public static string Char_E = "𝐄";
         public static string Char_Q = "𝙌";
