8213c7e04576bee04b78ababc3dc7ce5fe763481
peter.nguyen <peter.nguyen@hansencx.com>
2026-05-20 00:05:18 +0700

    Add button icon Filters

--- src/PowerTools.Core/Models/ButtonIcons.cs
+++ src/PowerTools.Core/Models/ButtonIcons.cs
@@ -16,6 +16,7 @@
         public static string Books = "📚";
         public static string ServerSide = "\ue2E0";
         public static string PenEdit = "\ue0D8";
+        public static string Filters = "\uE16E";
         public static string Char_K = "𝞙";
         public static string Char_E = "𝐄";
         public static string Char_Q = "𝙌";
