b85f6cf3cd00c440910d34d0e8631694415186b4
peter.nguyen <peter.nguyen@hansencx.com>
2026-07-08 23:26:31 +0700

    Enhance DataGrid and Button UI

---
+++ src/PowerTools.Core/Behaviours/ButtonBehavior.cs
@@ -0,0 +1,28 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace PowerTools.Core.Behaviours
+{
+    public static class ButtonBehavior
+    {
+        #region CornerRadius Property
+
+        public static CornerRadius GetCornerRadius(DependencyObject obj)
+        {
+            return (CornerRadius)obj.GetValue(CornerRadiusProperty);
+        }
+
+        public static void SetCornerRadius(DependencyObject obj, CornerRadius value)
+        {
+            obj.SetValue(CornerRadiusProperty, value);
+        }
+
+        public static readonly DependencyProperty CornerRadiusProperty = DependencyProperty.RegisterAttached(
+            "CornerRadius",
+            typeof(CornerRadius),
+            typeof(ButtonBehavior),
+            new FrameworkPropertyMetadata(new CornerRadius(5)));
+
+        #endregion
+    }
+}
