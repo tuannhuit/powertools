c5474de9c3d5e6a5cf617cb76aecb38c0056a35c
peter.nguyen <peter.nguyen@hansencx.com>
2026-07-12 00:19:03 +0700

    Enhance datagrid UI

---
+++ src/PowerTools.Core/Converters/BoolToStringConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace PowerTools.Core.Converters
+{
+    public class BoolToStringConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null || parameter == null) return value;
+
+            var strValue = value.ToString();
+            var values = parameter.ToString().Split(":");
+
+            return strValue.ToLower() == values[0].ToLower() ? values[1] : values[2];
+        }
+
+        public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
