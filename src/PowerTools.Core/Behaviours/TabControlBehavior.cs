d56bfc9b306f7f079319b6932e4ffc901a1539ff
peter.nguyen <peter.nguyen@hansencx.com>
2026-08-07 00:04:07 +0700

    Enhance UI

---
+++ src/PowerTools.Core/Behaviours/TabControlBehavior.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows;
+
+namespace PowerTools.Core.Behaviours
+{
+    public static class TabControlBehavior
+    {
+        #region CornerRadius Property
+
+        public static bool GetCanCloseTabItem(DependencyObject obj)
+        {
+            return (bool)obj.GetValue(CanCloseTabItemProperty);
+        }
+
+        public static void SetCanCloseTabItem(DependencyObject obj, bool value)
+        {
+            obj.SetValue(CanCloseTabItemProperty, value);
+        }
+
+        public static readonly DependencyProperty CanCloseTabItemProperty = DependencyProperty.RegisterAttached(
+            "CanCloseTabItem",
+            typeof(bool),
+            typeof(TabControlBehavior),
+            new FrameworkPropertyMetadata(true));
+
+        #endregion
+    }
+}
